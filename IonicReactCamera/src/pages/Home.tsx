02dd89cdcc90018d5519eec743700cb36d5edd1d
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-03-07 12:36:27 +0530

    update

--- IonicReactCamera/src/pages/Home.tsx
+++ IonicReactCamera/src/pages/Home.tsx
@@ -1,92 +1,105 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonImg,
+  IonFab,
+  IonFabButton,
+  IonIcon
+} from '@ionic/react';
 import React, { Component } from 'react';
 import { Plugins, CameraResultType } from '@capacitor/core';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import * as faceapi from 'face-api.js';
+
 const { Camera } = Plugins;
 const INITIAL_STATE = {
-  photo: '',
+  photo: ''
 };
+
 export class Home extends Component {
   state: any = {};
-  props: any = {};
   constructor(props: any) {
     super(props);
     this.state = { ...INITIAL_STATE };
     defineCustomElements(window);
-
   }
-  async componentDidMount() {
-    await this.loadModels();
-    console.log('loaded')
-
-}
-async loadModels () {
-    await faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
-    await faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
-    await faceapi.nets.ssdMobilenetv1.loadFromUri('../models')
-  }
-
   async takePicture() {
     const image = await Camera.getPhoto({
       quality: 90,
       allowEditing: false,
       resultType: CameraResultType.Uri
-      });
-      var imageUpload = image.webPath;
-      // Can be set to the src of an image now
-
-
-      this.setState({
+    });
+    var imageUpload = image.webPath;
+    // Can be set to the src of an image now
+    // await faceapi.nets.faceRecognitionNet.loadFromUri('./../models');
+    // await faceapi.nets.faceLandmark68Net.loadFromUri('./../models');
+    // await faceapi.nets.ssdMobilenetv1.loadFromUri('./../models');
+    this.setState({
       photo: imageUpload
-    })
+    });
+    console.log('image uploaded');
+    // this.start(this.state.photo);
   }
-
-
-
-  async function start() {
-    const container = document.createElement('div')
-    container.style.position = 'relative'
-    document.body.append(container)
-    const labeledFaceDescriptors = await loadLabeledImages()
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-    let image
-    let canvas
-    document.body.append('Loaded')
-    imageUpload.addEventListener('change', async () => {
-      if (image) image.remove()
-      if (canvas) canvas.remove()
-      image = await faceapi.bufferToImage(imageUpload.files[0])
-      container.append(image)
-      canvas = faceapi.createCanvasFromMedia(image)
-      container.append(canvas)
-      const displaySize = { width: image.width, height: image.height }
-      faceapi.matchDimensions(canvas, displaySize)
-      const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-      const resizedDetections = faceapi.resizeResults(detections, displaySize)
-      const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-      results.forEach((result, i) => {
-        const box = resizedDetections[i].detection.box
-        const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-        drawBox.draw(canvas)
-      })
-    })
+  async start(image) {
+    const container = document.getElementById('test');
+    const labeledFaceDescriptors = await this.loadLabeledImages();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+    let canvas;
+    image = await faceapi.bufferToImage(image);
+    container.append(image);
+    canvas = faceapi.createCanvasFromMedia(image);
+    container.append(canvas);
+    const displaySize = { width: image.width, height: image.height };
+    faceapi.matchDimensions(canvas, displaySize);
+    const detections = await faceapi
+      .detectAllFaces(image)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    const results = resizedDetections.map(d =>
+      faceMatcher.findBestMatch(d.descriptor)
+    );
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result.toString()
+      });
+      drawBox.draw(canvas);
+    });
   }
-
-  function loadLabeledImages() {
-    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  loadLabeledImages = () => {
+    const labels = [
+      'Black Widow',
+      'Captain America',
+      'Captain Marvel',
+      'Hawkeye',
+      'Jim Rhodes',
+      'Thor',
+      'Tony Stark'
+    ];
     return Promise.all(
       labels.map(async label => {
-        const descriptions = []
+        const descriptions = [];
         for (let i = 1; i <= 2; i++) {
-          const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
-          const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-          descriptions.push(detections.descriptor)
+          const img = await faceapi.fetchImage(
+            `https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`
+          );
+          await faceapi.nets.ssdMobilenetv1.loadFromUri('./../models');
+
+          const detections = await faceapi
+            .detectSingleFace(img, new faceapi.SsdMobilenetv1Options())
+            .withFaceLandmarks()
+            .withFaceDescriptor();
+          descriptions.push(detections.descriptor);
         }
 
-        return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        return new faceapi.LabeledFaceDescriptors(label, descriptions);
       })
-    )
-  }
+    );
+  };
 
   render() {
     const { photo } = this.state;
@@ -97,16 +110,25 @@ async loadModels () {
             <IonTitle>Ionic Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent className="ion-padding">
-          <IonImg style={{ 'border': '1px solid black', 'minHeight': '100px' }} src={photo} ></IonImg>
-          <IonFab color="primary" vertical="bottom" horizontal="center" slot="fixed">
-          <IonFabButton color="primary" onClick={() => this.takePicture()}>
-              <IonIcon name="add" />
+        <IonContent className='ion-padding'>
+          <IonImg
+            style={{ border: '1px solid black', minHeight: '100px' }}
+            src={photo}
+          ></IonImg>
+          <div className='test'></div>
+          <IonFab
+            color='primary'
+            vertical='bottom'
+            horizontal='center'
+            slot='fixed'
+          >
+            <IonFabButton color='primary' onClick={() => this.takePicture()}>
+              <IonIcon name='add' />
             </IonFabButton>
           </IonFab>
         </IonContent>
-      </IonPage >
+      </IonPage>
     );
-  };
+  }
 }
 export default Home;
