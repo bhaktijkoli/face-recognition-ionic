5bf660190205ffc154e46616317471925651a813
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-03-07 21:52:02 +0530

    update

--- IonicReactCamera/src/pages/Home.tsx
+++ IonicReactCamera/src/pages/Home.tsx
@@ -47,37 +47,64 @@ export class Home extends Component {
       photo: imageUpload
     });
     loadModel();
-    const img = `<img src = "${this.state.photo}"/>`;
+    const img = this.state.photo;
+    console.log(img);
     this.start(img);
   }
+
+  public getBase64Image(imgUrl, callback) {
+    var img = new Image();
+    // onload fires when the image is fully loadded, and has width and height
+    img.onload = function() {
+      var canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      var dataURL = canvas.toDataURL('image/png', 1.0),
+        dataURL = dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
+      callback(dataURL); // the base64 string
+    };
+    // set attributes and src
+    img.setAttribute('crossOrigin', 'anonymous'); //
+    img.src = imgUrl;
+  }
+
   async start(image) {
     console.log('Start started');
     const container = document.getElementById('test');
     const labeledFaceDescriptors = await this.loadLabeledImages();
     console.log(labeledFaceDescriptors);
     const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+    console.log(faceMatcher);
     let canvas;
-    image = await faceapi.bufferToImage(image);
-    container.append(image);
-    canvas = faceapi.createCanvasFromMedia(image);
-    container.append(canvas);
-    const displaySize = { width: image.width, height: image.height };
-    faceapi.matchDimensions(canvas, displaySize);
-    const detections = await faceapi
-      .detectAllFaces(image)
-      .withFaceLandmarks()
-      .withFaceDescriptors();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    const results = resizedDetections.map(d =>
-      faceMatcher.findBestMatch(d.descriptor)
-    );
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, {
-        label: result.toString()
-      });
-      drawBox.draw(canvas);
+    this.getBase64Image(image, function(base64image) {
+      console.log(base64image);
     });
+    // image = await faceapi.bufferToImage(image).catch(err => {
+    //   console.log(err);
+    // });
+    // console.log(image);
+    // container.append(image);
+    // canvas = faceapi.createCanvasFromMedia(image);
+    // container.append(canvas);
+    // const displaySize = { width: image.width, height: image.height };
+    // faceapi.matchDimensions(canvas, displaySize);
+    // const detections = await faceapi
+    //   .detectAllFaces(image)
+    //   .withFaceLandmarks()
+    //   .withFaceDescriptors();
+    // const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    // const results = resizedDetections.map(d =>
+    //   faceMatcher.findBestMatch(d.descriptor)
+    // );
+    // results.forEach((result, i) => {
+    //   const box = resizedDetections[i].detection.box;
+    //   const drawBox = new faceapi.draw.DrawBox(box, {
+    //     label: result.toString()
+    //   });
+    //   drawBox.draw(canvas);
+    // });
   }
   loadLabeledImages = () => {
     const labels = [
@@ -98,7 +125,7 @@ export class Home extends Component {
           );
           const model_path = window.location.origin + '/models';
           await faceapi.nets.ssdMobilenetv1.loadFromUri(model_path);
-
+          await faceapi.nets.faceLandmark68Net.loadFromUri(model_path);
           const detections = await faceapi
             .detectSingleFace(img, new faceapi.SsdMobilenetv1Options())
             .withFaceLandmarks()
@@ -124,8 +151,11 @@ export class Home extends Component {
           <IonImg
             style={{ border: '1px solid black', minHeight: '100px' }}
             src={photo}
+            id='img'
           ></IonImg>
-          <div id='test'></div>
+          <div id='test'>
+            <canvas id='canvas'></canvas>
+          </div>
           <IonFab
             color='primary'
             vertical='bottom'
